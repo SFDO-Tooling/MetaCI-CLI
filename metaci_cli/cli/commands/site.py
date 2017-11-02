c3ab5c8f27e431104402841a1baa0558023a81da
Jason Lantz <jlantz@salesforce.com>
2017-11-01 21:15:12 -0500

    Switch back to master of MetaCI

--- metaci_cli/cli/commands/site.py
+++ metaci_cli/cli/commands/site.py
@@ -146,7 +146,7 @@ def site_add(config, name, shape):
     payload = {
         'app': {},
         'source_blob': {
-            'url': 'https://github.com/SalesforceFoundation/mrbelvedereci/tarball/feature/redis-pooling/',
+            'url': 'https://github.com/SalesforceFoundation/mrbelvedereci/tarball/master/',
        },
     }
     env = {} 
